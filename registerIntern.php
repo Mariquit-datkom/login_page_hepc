b93be2e226c05ff4150b596ab546f159ca831524
Lance Mariquit <lancemariquit.1214@gmail.com>
2026-02-09 10:07:13 +0800

    Added password confirmation before registering

--- registerIntern.php
+++ registerIntern.php
@@ -35,48 +35,8 @@
     <title>Register an Intern</title>
 </head>
 <body>
-    <!-- Page Header -->
-    <header>
-        <div class="header-left">
-            <img src="assets/company_logo.png" alt="company_logo" class="company-logo">
-            <span class="dashboard-title"> Register Intern Account </span>
-        </div>
-
-        <div class="header-right">
-            <span class="username"> <?php echo htmlspecialchars($currentUser); ?> </span>
-            <div class="user-menu-container">
-                <img src="assets/user_avatar.png" alt="user_avatar" class="user-avatar" id="user-avatar-btn">
-                <div class="dropdown-content" id="user-avatar-dropdown">
-                    <a href="logout.php">Logout</a>
-                </div>
-            </div>
-        </div>
-    </header>
-
-    <!-- Page Navigation Bar -->
-    <nav class="nav-bar">
-        <div class="nav-links">
-            <a href="adminDashboard.php" class="nav-item 
-            <?php echo ($currentPage === 'adminDashboard.php') ? 'active' : ''; ?>" 
-            data-text="Dashboard"><i class="fa fa-home"></i>Dashboard
-            </a>
-
-            <a href="internsListPage.php" class="nav-item 
-            <?php echo ($currentPage === 'internsListPage.php') ? 'active' : ''; ?>" 
-            data-text="Interns List"><i class="fa fa-clipboard-list"></i>Interns List
-            </a>
-
-            <a href="requestsPage.php" class="nav-item
-            <?php echo ($currentPage === 'requestsPage.php') ? 'active' : ''; ?>" 
-            data-text="Requests"><i class="fa fa-question"></i>Requests
-            </a>
-
-            <a href="javascript:void(0)" class="nav-item 
-            <?php echo ($currentPage === 'registerIntern.php') ? 'active' : ''; ?>" 
-            data-text="Register Intern"><i class="fa fa-pencil"></i>Register Intern
-            </a>
-        </div>
-    </nav>
+    <!-- Generate Page Header and Nav Bar -->
+     <?php include 'adminHeaderAndNav.php'; ?>
     
     <!-- Registration Parent Container -->
     <div class="registrationContainer">
@@ -95,6 +55,12 @@
                 <div class="formGroup"> <!-- Password Input -->
                     <label for="password" class="formLabel"> Password: </label>  
                     <input type="text" class="formInput" id="password" name="password" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="formGroup"> <!-- Password Confirmation -->
+                    <label for="confirm-password" class="formLabel"> Confirm Password: </label>  
+                    <input type="text" class="formInput" id="confirm-password" name="confirm-password" required>
                 </div>
             </div>
             <div class="registerButtonContainer">
--- registrationAuth.php
+++ registrationAuth.php
@@ -6,16 +6,20 @@
     // Form Submission Authentication
     if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
         $username = $_POST['username'];
-        $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // Password hashing for better security
+        $password = $_POST['password'];
+        $confirm_password = $_POST['confirm-password'];
 
         // Checks for existing username
         $checkstmt = $pdo->prepare("SELECT * FROM users WHERE username = :username");
         $checkstmt->execute(['username' => $username]);
         $usernameExists = $checkstmt->fetchColumn();
 
-        if ($usernameExists) {
+        if ($password !== $confirm_password) {
+            $_SESSION['registration_msg'] = "<p style='color: red;'>Passwords don't match</p>";
+        } else if ($usernameExists){
             $_SESSION['registration_msg'] = "<p style='color: red;'>Username already taken. Please choose another.</p>";
         } else {
+            $hashed_password = password_hash($password, PASSWORD_BCRYPT);
             $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
             $stmt = $pdo->prepare($sql);
 
@@ -30,7 +34,7 @@
             } else {
                 $_SESSION['registration_msg'] = "<p style='color: red;'>Error during registration.</p>";
             }
-        }
+        }        
 
         // Self-redirect to show confirmation message
         header("Location: registerIntern.php");
